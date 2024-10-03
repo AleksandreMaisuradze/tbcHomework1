ab7409b802110681e44c11e0d8a10041adf9accc
Aleksandre Maisuradze <aleksandremaisuradze11@gmail.com>
2024-10-03 20:49:18 +0400

    correction

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -5,9 +5,9 @@ export default function ProfileCard(props) {
     return (
         <div className='profile_card'>
             <img src={props.image} className='ProfileCardImage' />
-            <input type='text' placeholder={props.name} className='input' />
-            <input type='text' placeholder={props.surname} className='input'/>
-            <input type='email'placeholder={props.email} className='input'/>
+            <input type='text' placeholder={props.name} className='input' readOnly />
+            <input type='text' placeholder={props.surname} className='input' readOnly/>
+            <input type='email'placeholder={props.email} className='input' readOnly/>
         </div>
     )
 }
